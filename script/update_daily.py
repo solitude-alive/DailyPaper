4f78ccb146f07ab78299c73a7847e7a923355895
zgyang <thisyzg@gmail.com>
2025-01-22 00:56:35 +0800

    Init commit.

---
+++ script/update_daily.py
@@ -0,0 +1,24 @@
+def update_daily_papers(hl_papers, papers, date):
+    """
+    Updates the DailyPaper.md file with the highlight papers.
+    Target format:
+        # The Latest Daily Papers - Date: {date}
+        ## Highlight Papers
+        ### **[Title: {paper['title']}]({paper['link']})**
+        - **Summary**: {paper['summary']}
+        - **Score**: {paper['score']}/10
+        ...
+        ## Other Papers
+    """
+    file = "DailyPaper.md"
+
+    with open(file, "w") as f:
+        f.write(f"# The Latest Daily Papers - Date: {date}\n")
+        f.write(f"## Highlight Papers\n")
+        for paper in hl_papers:
+            f.write(f"### **[Title: {paper['title']}]({paper['link']})**\n")
+            f.write(f"- **Summary**: {paper['summary']}\n")
+            f.write(f"- **Score**: {paper['score']}/10\n\n")
+        f.write(f"## Other Papers\n")
+        for paper in papers:
+            f.write(f"### **[Title: {paper['title']}]({paper['link']})**\n")
